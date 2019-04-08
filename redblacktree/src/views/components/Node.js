1e110aea0c9ad4fa3b98d46ede9ca0d1e72827ee
Seongmin Park <seongminnpark@gmail.com>
2019-04-08 14:54:50 +0000

    Initial render

--- redblacktree/src/views/components/Node.js
+++ redblacktree/src/views/components/Node.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import './Styles/Node.css' 
 
-export default class Node extends Component{
+const NODE_SIZE = 50;
+
+class Node extends Component{
 
     render() {
         
+        var left = this.props.index * NODE_SIZE; 
+        var top = this.props.level * NODE_SIZE; 
+        var right = left + NODE_SIZE;
+        var bottom = top + NODE_SIZE;
+
+        var styles = {
+            position: 'absolute',
+            left: left + 'px',
+            top: top + 'px',
+            width: NODE_SIZE,
+            height: NODE_SIZE
+        };
+
         return (
-           <div className='node'></div>
+           <div className='node' 
+                style={styles}></div>
         )
     }
 }
+
+Node.propTypes = {
+    id: PropTypes.string,
+    level: PropTypes.number,
+    index: PropTypes.number
+};
+
+export default Node;
--- redblacktree/src/views/components/Tree.js
+++ redblacktree/src/views/components/Tree.js
@@ -6,18 +6,49 @@ import Node from './Node';
 
 export default class Tree extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            nodeMap: {}
+        }
+    }
+
     componentDidMount() {
-        
-        console.log(new TreeCore())
+        var tree = new TreeCore(); 
+        tree.insert(1);
+        tree.insert(2);
+        tree.insert(3);
+        var nodeMap = tree.compile();
+        this.setState({
+            nodeMap: nodeMap
+        });
     }
 
     render() {
+        
+        const nodes = [];
+        const paths = [];
+
+        const nodeMap = this.state.nodeMap;
+        
+        Object.getOwnPropertyNames(nodeMap).forEach(key => {
+            let value = nodeMap[key];
+            for (var i=0; i < value.length; i++) {
+                var node = (<Node id={value[i]}
+                                  level={parseInt(key)} 
+                                  index={i} 
+                                  key={value[i]}/>)
+                nodes.push(node);
+            }
+        });
+
         return (
-
+             
             <div className='tree'> 
-                
-                                  
-
+            
+                { nodes }
+                { paths }
+                                 
             </div>
         )
 
