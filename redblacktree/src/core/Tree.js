818e84aa8cd849488e15123104de8e95d5ad3ab8
Seongmin Park <seongminnpark@gmail.com>
2019-04-08 02:43:20 +0000

    Finish insert

--- redblacktree/src/core/Tree.js
+++ redblacktree/src/core/Tree.js
@@ -1,15 +1,58 @@
 export class Node {
+    
+    static get ROOT() { return 'ROOT'; }
+    static get LEFT() { return 'LEFT'; }
+    static get RIGHT() { return 'RIGHT'; }
+
+    static get RED() { return 'RED'; }
+    static get BLACK() { return 'BLACK'; }
 
     constructor() {
         this.data = null;
         this.leftChild = null;
         this.rightChild = null;
-        this.nodePath = null;
+        this.parent = null;
+        this.direction = null;
         this.count = 1;
+        this.color = Node.BLACK;
     }
 
-    isLeaf() {
-        return this.leftChild == null && this.rightChild == null;
+    recolor() {
+        if (this.color === Node.RED) {
+            this.color = Node.BLACK;
+        } else {
+            this.color = Node.RED;
+        }
+    }
+
+    get isRoot() {
+        return this.parent == null;
+    }
+
+    get grandParent() {
+        if (this.isRoot) {
+            return null;
+        } else {
+            return this.parent.parent;
+        }
+    }
+
+    get uncle() {
+        var grandParent = this.grandParent;
+
+        if (this.isRoot) {
+            return null;
+
+        } else if (grandParent == null) {
+            return null;
+
+        } else {
+            if (grandParent.leftChild === this.parent) {
+                return grandParent.rightChild;
+            } else {
+                return grandParent.leftChild;
+            }
+        }
     }
 
 }
@@ -21,7 +64,127 @@ export default class Tree {
         this.nodeMap = {};
         this.logger = new TreeLogger();
     }
+    
+    // Getter functions. 
+    
+    getNode(nodeId) {
+        return this.nodeMap[nodeId];
+    }
 
+    // Validity functions.
+    
+    fix(jobId) {
+        while(this.fixNodes(jobId, this.root)) {
+        
+        } 
+    }
+
+    fixNodes(jobId, node) { 
+        var curr = this.fixNode(jobId, node);
+        var left = node.leftChild == null ? 
+            false : this.fixNodes(jobId, node.leftChild);
+        var right = node.rightChild == null ? 
+            false : this.fixNodes(jobId, node.rightChild);
+        return curr || left || right;
+    }
+
+    fixNode(jobId, node) {
+
+        if (node == null || node.color !== Node.RED) {
+            return false;
+        } 
+        
+        var parent = node.parent;
+        var grandParent = node.grandParent;
+        var uncle = node.uncle;
+       
+        if (node.isRoot && node.color === Node.RED) { 
+            // Case 1 - Node is root.
+            this.logger.log(jobId, TreeLogger.ERROR, node.id, null, 1); 
+            this.recolor(jobId, node);
+            return true;
+            
+        } else if (uncle != null && uncle.color === Node.RED) { 
+            // Case 2 - Uncle is red.
+            this.logger.log(jobId, TreeLogger.ERROR, node.id, null, 2); 
+            parent.recolor();
+            grandParent.recolor();
+            uncle.recolor();
+            return true;
+        
+        } else if (uncle == null || uncle.color === Node.BLACK) {  
+            if (parent.color === Node.RED && 
+                parent.direction !== node.direction) {
+                // Case 3 - Uncle is black and triangle exists.
+                this.logger.log(jobId, TreeLogger.ERROR, node.id, null, 3); 
+                if (node.direction === Node.LEFT) {
+                    this.rotateRight(jobId, parent);
+                } else {
+                    this.rotateLeft(jobId, parent);
+                }
+                return true; 
+                
+            } else if (parent.color === Node.RED && 
+                parent.direction === node.direction) {
+                // Case 4 - Unlce is black and line exists.
+                this.logger.log(jobId, TreeLogger.ERROR, node.id, null, 4); 
+                if (node.direction === Node.LEFT) {
+                    this.rotateRight(jobId, grandParent);
+                } else {
+                    this.rotateLeft(jobId, grandParent);
+                }
+                this.recolor(jobId, grandParent);
+                this.recolor(jobId, parent);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    recolor(jobId, node) {
+        node.recolor();
+        this.logger.log(jobId, TreeLogger.RECOLOR, node.id, null, node.color); 
+    }
+
+    // Rotation functions.
+    
+    rotateLeft(jobId, node) {
+        this.logger.log(jobId, TreeLogger.ROTATE, node.id, null, Node.LEFT); 
+
+        var rightChild = node.rightChild;
+        var parent = node.parent;
+        var direction = node.direction;
+        var rightChildLeftChild = rightChild.leftChild;
+
+        node.parent = rightChild;
+        node.direction = Node.LEFT;
+        node.rightChild = rightChildLeftChild;
+
+        rightChild.parent = parent; 
+        rightChild.direction = direction;
+        rightChild.leftChild = node;
+    }
+
+    rotateRight(jobId, node) {
+        this.logger.log(jobId, TreeLogger.ROTATE, node.id, null, Node.RIGHT); 
+        
+        var leftChild = node.leftChild;
+        var parent = node.parent;
+        var direction = node.direction;
+        var leftChildRightChild = leftChild.rightChild;
+
+        node.parent = leftChild;
+        node.direction = Node.RIGHT;
+        node.leftChild = leftChildRightChild;
+
+        leftChild.parent = parent; 
+        leftChild.direction = direction;
+        leftChild.rightChild = node;
+    }
+
+    // Visualization functions.
+    
     print() {
         this.printSubtree(this.root);
     }
@@ -30,28 +193,30 @@ export default class Tree {
         if (node == null) {
             return;
         }
-        console.log(node.data);
+        console.log(node.data, node.color);
         this.printSubtree(node.leftChild);
         this.printSubtree(node.rightChild);
-    }
+    } 
 
-    isBalanced() {
-        if (this.root == null) {
-            return true;
-        }
-    }
-
+    // Insertion functions. 
+    
     insert(data) {
         var jobId = data.toString();
         this.logger.createEvent(jobId);
-        this.bstInsert(jobId, this.root, data);   
+        
+        // Insert node and color it red.
+        var newId = this.bstInsert(jobId, this.root, data);    
+        var newNode = this.getNode(newId);
+        newNode.color = Node.RED; 
 
-    }
+        // Satisfy constraints.
+        this.fix(jobId); 
+    } 
 
     bstInsert(jobId, node, data) {
 
         if (node == null) {
-            this.insertAt(jobId, node, 'ROOT', data);
+            return this.insertAt(jobId, node, Node.ROOT, data);
 
         } else {
 
@@ -60,52 +225,54 @@ export default class Tree {
             if (node.data === data) {
                 this.logger.log(jobId, TreeLogger.COMPARE, node.id, null, null);
                 node.count += 1;
+                return null;
 
             } else if (node.data > data) {
 
                 if (node.leftChild == null) {
-                    this.insertAt(jobId, node, 'LEFT', data);
+                    return this.insertAt(jobId, node, Node.LEFT, data);
                 } else {
-                    this.bstInsert(jobId, node.leftChild, data);
+                    return this.bstInsert(jobId, node.leftChild, data);
                 }
 
             } else {
 
                 if (node.rightChild == null) {
-                    this.insertAt(jobId, node, 'RIGHT', data); 
+                    return this.insertAt(jobId, node, Node.RIGHT, data); 
                 } else {
-                    this.bstInsert(jobId, node.rightChild, data);
+                    return this.bstInsert(jobId, node.rightChild, data);
                 }
             }
         }
 
     }
 
-    insertAt(jobId, parentNode, direction, data) {
+    insertAt(jobId, parent, direction, data) {
 
         var newNode = new Node();
         newNode.data = data;
+        newNode.parent = parent;
         var newId = Math.random().toString(36).slice(2);
         newNode.id = newId;
+        newNode.direction = direction;
         this.nodeMap[newId] = newNode;
         
-        var parentId = parentNode == null ? null : parentNode.id;
+        var parentId = parent == null ? null : parent.id;
 
         this.logger.log(jobId, TreeLogger.INSERT, 
             newId, parentId, direction);
 
-        if (direction === 'ROOT') {
-            newNode.nodePath = '';
+        if (direction === Node.ROOT) {
             this.root = newNode; 
 
-        } else if (direction === 'LEFT') {
-            newNode.nodePath = parentNode.nodePath + 'L'; 
-            parentNode.leftChild = newNode;
+        } else if (direction === Node.LEFT) {
+            parent.leftChild = newNode;
 
-        } else if (direction === 'RIGHT') {
-            newNode.nodePath = parentNode.nodePath + 'R';
-            parentNode.rightChild = newNode; 
+        } else if (direction === Node.RIGHT) {
+            parent.rightChild = newNode; 
         }
+
+        return newId;
 
     }
 
@@ -115,6 +282,9 @@ export class TreeLogger {
 
     static get LOOK() { return 'LOOK'; }
     static get INSERT() { return 'INSERT'; }
+    static get ROTATE() { return 'ROTATE'; }
+    static get ERROR() { return 'ERROR'; }
+    static get RECOLOR() { return 'RECOLOR'; }
 
     constructor() {
         this.logs = {};
