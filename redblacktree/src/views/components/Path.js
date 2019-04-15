d2b99597036b5d40f8ed60d61650f6d1efcaa36c
Seongmin Park <seongminnpark@gmail.com>
2019-04-15 09:38:46 +0000

    Add paths

--- redblacktree/src/views/components/Path.js
+++ redblacktree/src/views/components/Path.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Line } from 'react-lineto';
+
 
 import { TweenMax } from 'gsap';
 
@@ -9,25 +11,30 @@ class Path extends Component {
 
    componentDidUpdate(prevProps, prevState) {
        const el = this.container;
+        console.log('here')
+        console.log(this.props.fromX, this.props.fromY, this.props.toX, this.props.toY);
        TweenMax.fromTo(el, 0.3, 
-           {attr:{x1: this.props.prevFromX, y1: this.props.prevFromY,
-            x2: this.props.prevToX, y2: this.props.prevToY}},
+           {x0: this.props.prevFromX, y0: this.props.prevFromY,
+            x1: this.props.prevToX, y1: this.props.prevToY},
 
-           {attr:{x1: this.props.fromX, y1: this.props.fromY,
-            x2: this.props.toX, y2: this.props.toY}}
+           {x0: this.props.fromX, y0: this.props.fromY,
+            x1: this.props.toX, y1: this.props.toY}
        );
    }
 
     componentDidMount() {
         const el = this.container;
 
+        console.log('here1')
+        console.log(this.props.fromX, this.props.fromY, this.props.toX, this.props.toY);
+
         TweenMax.fromTo(el, 0.3, 
-           {attr:{x1: this.props.fromX, y1: this.props.fromY,
-            x2: this.props.toX, y2: this.props.toY},
+           {x0: this.props.fromX, y0: this.props.fromY,
+            x1: this.props.toX, y1: this.props.toY,
            opacity: 0},
 
-           {attr:{x1: this.props.fromX, y1: this.props.fromY,
-            x2: this.props.toX, y2: this.props.toY},
+           {x0: this.props.fromX, y0: this.props.fromY,
+            x1: this.props.toX, y1: this.props.toY,
            opacity: 1}
        );
 
@@ -42,20 +49,11 @@ class Path extends Component {
 
         return (
 
-            <svg className='path' 
-                
-                style={{
-                    position:'absolute',
-                }}
-            >
-
-            <line ref={c => this.container = c} style={styles} 
-                x1={this.props.fromX} 
-                y1={this.props.fromY} 
-                x2={this.props.toX} 
-                y2={this.props.toY}/>
-
-            </svg>
+           <Line x0={this.props.fromX} y0={this.props.fromY} 
+            x1={this.props.toX} y1={this.props.toY} 
+            ref={c => this.container = c} 
+            className='path'
+            style={styles}/>
         )
     }
     
